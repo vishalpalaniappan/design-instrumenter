3888d73e60798abe272d3054c63f874a13710ed1
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-01 10:06:02 -0400

    Create output folder if it doesn't exist and print confirmation.

--- src/parser.py
+++ src/parser.py
@@ -77,7 +77,16 @@ def parse_source_file(source_path):
                 "source": source,
             })
                 
+    # File Name
     nameWExtenstion = Path(source_path).name
     name = os.path.splitext(nameWExtenstion)[0] 
-    with open(f"./output/{name}_mapping.json", "w") as mapping_file:
+
+    # Output path (create folder if it doesn't exist)
+    path = Path("./output")
+    path.mkdir(parents=True, exist_ok=True)
+    output_path = path / f"{name}_mapping.json"
+
+    with open(output_path, "w") as mapping_file:
         json.dump(mapping, mapping_file, indent=4)
+
+    print(f"Mapping generated for {source_path} and saved to {output_path}")
