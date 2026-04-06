a1d1e351c60d0c1f4c196ff933b098cf6b86774f
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-06 14:34:16 -0400

    Add skeleton for logging helper function.

---
+++ src/LoggingHelper.py
@@ -0,0 +1,44 @@
+import logging
+from pathlib import Path
+from clp_logging.handlers import ClpKeyValuePairStreamHandler
+
+import os
+import uuid
+
+ADLI_EXECUTION_ID = str(uuid.uuid4())
+
+path = Path(os.path.dirname(__file__)) / f"{ADLI_EXECUTION_ID}.clp.zst"
+clp_handler = ClpKeyValuePairStreamHandler(open(path, "wb"))
+logger = logging.getLogger("adli")
+logger.setLevel(logging.INFO)
+logger.addHandler(clp_handler)
+
+class LoggingHelper:
+    '''
+        This class holds all the logging functions used by the 
+        instrumented code during runtime. 
+    '''
+
+    def __init__(self):
+        self.count = 0
+
+    def logVariable(self):
+        pass
+
+    def logStmt(self):
+        pass
+
+    def logException(self):
+        pass
+
+    def logHeader(self):
+        pass
+
+    def logOutput(self):
+        pass
+
+    def decodeInput(self):
+        pass
+
+
+adli = LoggingHelper()
