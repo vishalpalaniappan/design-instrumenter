24632f0df6449f351de05b8a7728b535b8186e6c
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-06 14:19:46 -0400

    Write instrumented output to file.

--- src/instrumenter.py
+++ src/instrumenter.py
@@ -1,15 +1,34 @@
-import json, ast
+import os
+import json
+import ast
 
 class SourceInstrumenter(ast.NodeTransformer):
     def __init__(self, mapping_data):
+        '''
+            Initialize the SourceInstrumenter with the mapping data.
+        '''
         self.mapping_data = mapping_data
 
-    def visit(self, node):
+
+    def getEntryForNode(self, node):
+        '''
+            Get the mapping entry for a given AST node.
+        '''
         for entry in self.mapping_data:
             if (hasattr(node, "lineno") and hasattr(node, "end_lineno") and 
                 node.lineno == entry["start_line"] and node.end_lineno == entry["end_line"]):
-                print(entry["uid"])
-                return node
+                return entry
+            
+        return None
+
+    def visit(self, node):
+        '''
+            Override the visit method to inject instrumentation code based on the mapping data.
+        '''
+        entry = self.getEntryForNode(node)
+        if entry:
+            print(entry["uid"], type(node).__name__, entry["type"])
+            return node
             
         self.generic_visit(node) 
         return node
@@ -27,3 +46,7 @@ def instrument_semantic_information(source, mapping):
     instrumenter = SourceInstrumenter(mapping_data)
     instrumented_tree = instrumenter.visit(tree)
     instrumented_code = ast.unparse(instrumented_tree)
+
+    filename = os.path.basename(source)
+    with open("./output/instrumented_" + filename, "w") as instrumented_file:
+        instrumented_file.write(instrumented_code)
