2e34ba4111aea84d016ea1a47dbac07b3d2bb2b9
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-11 18:12:26 -0400

    Add import statement to instrumented sources body.

--- src/instrumenter.py
+++ src/instrumenter.py
@@ -3,35 +3,6 @@ import json
 import ast
 from src.instrumentation.LogInjector import LogInjector
 
-class SourceInstrumenter(ast.NodeTransformer):
-    def __init__(self, mapping_data):
-        '''
-            Initialize the SourceInstrumenter with the mapping data.
-        '''
-        self.mapping_data = mapping_data
-
-    def getEntryForNode(self, node):
-        '''
-            Get the mapping entry for a given AST node.
-        '''
-        for entry in self.mapping_data:
-            if (hasattr(node, "lineno") and hasattr(node, "end_lineno") and 
-                node.lineno == entry["start_line"] and node.end_lineno == entry["end_line"]):
-                return entry
-            
-        return None
-
-    def visit(self, node):
-        '''
-            Override the visit method to inject instrumentation code based on the mapping data.
-        '''
-        entry = self.getEntryForNode(node)
-        if entry:
-            print(entry["uid"], type(node).__name__, entry["type"])
-            
-        self.generic_visit(node) 
-        return node
-
 def instrument_semantic_information(source):
     with open(source, "r") as source_file:
         source_code = source_file.read()
@@ -43,6 +14,9 @@ def instrument_semantic_information(source):
         source = package[file]["content"]
         injector = LogInjector(stmtIndex)
         new_tree = injector.visit(ast.parse(source))
+
+        importNode = ast.parse("from LoggingHelper import adli").body[0]
+        new_tree.body.insert(0, importNode)
 
         with open("./output/instrumented_" + os.path.basename(package[file]["name"]), "w") as instrumented_file:
             instrumented_file.write(ast.unparse(new_tree))
