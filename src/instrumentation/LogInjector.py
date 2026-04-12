5645a7f9dc8c143c4726cfc882b6741de458cd0f
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-12 06:06:22 -0400

    Add variable logging.

--- src/instrumentation/LogInjector.py
+++ src/instrumentation/LogInjector.py
@@ -23,7 +23,7 @@ class LogInjector(ast.NodeTransformer):
 
 
     def getLogStmts (self):
-        return ast.Expr(
+        behaviorLog =ast.Expr(
             value=ast.Call(
                 func=ast.Attribute(
                     value=ast.Name(id="adli", ctx=ast.Load()),
@@ -37,6 +37,26 @@ class LogInjector(ast.NodeTransformer):
                 keywords=[]
             )
         )
+
+        variableLogs = []
+        for participant in self.entry["_participants"]:
+            variableLogs.append(ast.Expr(
+                value=ast.Call(
+                    func=ast.Attribute(
+                        value=ast.Name(id="adli", ctx=ast.Load()),
+                        attr="logVariable",
+                        ctx=ast.Load()
+                    ),
+                    args=[
+                        ast.Constant(value=self.entry["_uid"]),
+                        ast.Constant(value=participant["participantName"]),
+                        ast.Name(id=participant["variableName"], ctx=ast.Load())
+                    ],
+                    keywords=[]
+                )
+            ))
+
+        return [variableLogs, behaviorLog]
 
     def visit(self, node):
         self.entry = is_in_map(node, self.mapping)
